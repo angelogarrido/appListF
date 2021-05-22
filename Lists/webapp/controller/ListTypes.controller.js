6a77dc8d8892573da19d1679e089f410c6985ecc
angelogarrido <angelo.garrido.m@gmail.com>
2021-05-22 14:36:43 +0000

    appList

---
+++ Lists/webapp/controller/ListTypes.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+		"sap/ui/core/mvc/Controller"
+	],
+	/**
+     * @param {typeof sap.ui.core.mvc.Controller} Controller
+     */
+	function (Controller) {
+		"use strict";
+
+		return Controller.extend("logaligroup.Lists.controller.ListTypes", {
+			onInit: function () {
+                var oJSONModel = new sap.ui.model.json.JSONModel();
+                oJSONModel.loadData("./localService/mockdata/ListData.json");
+                this.getView().setModel(oJSONModel);
+
+            }
+		});
+	});
